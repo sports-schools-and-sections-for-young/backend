c13651f753dcec79d69b13128e77b0fa326e7749
Faye Billye <fbillye@student.21-school.ru>
2023-11-03 15:02:56 +0700

    Добавил админку

--- backend/organizations/admin.py
+++ backend/organizations/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Address, Order, Phone_number, Phone_of_organization,
+                     Rewiev, Sport_organization)
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('index', 'city', 'metro', 'district',
+                    'street', 'house')
+    list_filter = ('index', 'city', 'metro', 'district',
+                   'street', 'house')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Sport_organization)
+class Sport_organizationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'logo', 'address', 'email',
+                    'site', 'description')
+    list_filter = ('title', 'logo', 'address', 'email',
+                   'site')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('sport_organization', 'fio', 'age', 'gender',
+                    'phone', 'comment')
+    list_filter = ('sport_organization', 'fio', 'age', 'gender',
+                   'phone')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Phone_number)
+class Phone_numberAdmin(admin.ModelAdmin):
+    list_display = ('value', 'comment')
+    list_filter = ('value', )
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Phone_of_organization)
+class Phone_of_organizationAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'sport_school')
+    list_filter = ('phone', 'sport_school')
+
+
+@admin.register(Rewiev)
+class RewievAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'date_and_time', 'rating', 'sport_school')
+    list_filter = ('date_and_time', 'rating', 'sport_school')
+    empty_value_display = '-пусто-'
