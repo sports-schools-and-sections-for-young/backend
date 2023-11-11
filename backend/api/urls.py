195987aafebe62880419336f3912a43754c6449c
Faye Billye <fbillye@student.21-school.ru>
2023-11-11 19:11:31 +0700

    Исправил ошибки линтера

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,21 +1,12 @@
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from django.urls import path
-from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import SearchSectionViewSet, SportTypeViewSet
 
 app_name = 'api'
-
-router = DefaultRouter()
-
-# Эндпойнт для отображения всех видов спорта из БД
-router.register('sport_types', SportTypeViewSet, basename='sport_types')
-# Эндпойнт для поиска секций
-router.register('search_sections', SearchSectionViewSet,
-                basename='search_sections')
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -26,6 +17,14 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=(permissions.AllowAny,)
 )
+
+router = DefaultRouter()
+
+# Эндпойнт для отображения всех видов спорта из БД
+router.register('sport_types', SportTypeViewSet, basename='sport_types')
+# Эндпойнт для поиска секций
+router.register('search_sections', SearchSectionViewSet,
+                basename='search_sections')
 
 urlpatterns = [
     path('', include(router.urls)),
