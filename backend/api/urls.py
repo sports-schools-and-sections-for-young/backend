bc0436ea1c70256bd4a31b42644ef26940c8e0b3
Faye Billye <fbillye@student.21-school.ru>
2023-11-11 19:09:24 +0700

    Добавил readme, redoc, swagger

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,3 +1,7 @@
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+from django.urls import path
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -13,8 +17,23 @@ router.register('sport_types', SportTypeViewSet, basename='sport_types')
 router.register('search_sections', SearchSectionViewSet,
                 basename='search_sections')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sport Hub API",
+        default_version='v1',
+        description="API documentation"
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,)
+)
+
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+
 ]
