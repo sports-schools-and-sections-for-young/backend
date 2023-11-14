5994620a18ae63825bc36c955b96da40af0dba3e
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-11-14 22:22:03 +0300

    search_view_3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,46 +1,43 @@
 from django_filters.rest_framework import FilterSet, filters
-
 from sections.models import Section, SportType
 
 
-class SearchFilter(FilterSet):
-    """Фильтр по полям секции."""
-    gender = filters.CharFilter(lookup_expr='startswith')
-    sport_type = filters.ModelChoiceFilter(
-        queryset=SportType.objects.all(),
-        field_name='title'
+class SearchSectionFilter(FilterSet):
+    """
+    Фильтр для поиска секций по следующим полям: пол ребенка, вид спорта,
+    возраст ребенка, стоимость занятий, адрес секции.
+    """
+    age_group = filters.NumberFilter(method='get_age_group')
+    sport_type = filters.ModelMultipleChoiceFilter(
+        queryset=SportType.objects.all()
     )
-    age_group = filters.NumberFilter()
-    age_group__gte = filters.NumberFilter(
-        field_name='age_group',
-        lookup_expr='gte'
-    )
-    age_group__lte = filters.NumberFilter(
-        field_name='age_group',
-        lookup_expr='lte'
-    )
-    price = filters.NumberFilter()
-    price__gte = filters.NumberFilter(
-        field_name='price',
-        lookup_expr='gte'
-    )
-    price__lte = filters.NumberFilter(
-        field_name='price',
-        lookup_expr='lte'
-    )
+    price = filters.NumberFilter(method='get_price')
+    address = filters.CharFilter(method='get_address')
 
     class Meta:
         model = Section
-        fields = ('gender', 'sport_type', 'age_group', 'price')
+        fields = ('gender', 'sport_type', 'age_group', 'price', 'address')
+
+    # Фильтр по возрасту ребенка
+    def get_age_group(self, queryset, name, value):
+        return queryset.filter(
+            age_group__year_until__gte=value, age_group__year_from__lte=value)
+
+    # Фильтр по стоимости занятий
+    def get_price(self, queryset, name, value):
+        return queryset.filter(price__gte=0, price__lte=value)
+
+    # Фильтр по адресу секции
+    def get_address(self, queryset, name, value):
+        for item in value.split():
+            queryset = queryset.filter(
+                address__full_address__icontains=item)
+        return queryset
 
 
 class SportTypeFilter(FilterSet):
     """Фильтр по видам спорта."""
-    sport_type = filters.ModelChoiceFilter(
-        queryset=SportType.objects.all(),
-        field_name='title'
-    )
 
     class Meta:
         model = SportType
-        fields = ('sport_type', )
+        fields = ('title', )
