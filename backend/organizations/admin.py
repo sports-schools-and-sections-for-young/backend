f0f0c186e0a191d702186aca228f00c2bdb21bf5
Faye Billye <fbillye@student.21-school.ru>
2023-12-13 15:05:25 +0700

    Обновил админку

--- backend/organizations/admin.py
+++ backend/organizations/admin.py
@@ -6,9 +6,15 @@ from .models import PhoneNumber, PhoneOfOrganization, SportOrganization
 
 @admin.register(SportOrganization)
 class SportOrganizationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'address', 'email', 'site', 'description')
-    list_filter = ('title', 'address', 'email', 'site')
+    list_display = ('title', 'get_username', 'address', 'email', 'site',
+                    'description')
+    list_filter = ('title', 'user__username', 'address', 'email', 'site')
     empty_value_display = settings.EMPTY_VALUE
+
+    # Отображение владельца спортшколы в админке
+    def get_username(self, obj):
+        return obj.user.username
+    get_username.short_description = 'Владелец'
 
 
 @admin.register(PhoneNumber)
