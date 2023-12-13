005476906520f6cd46a13ac73e7b382feb458d38
Alena Sapfirova <sapfirova.alena@yandex.ru>
2023-12-14 00:23:57 +0300

    correct_1

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -10,5 +10,5 @@ admin.site.unregister(Group)
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff','id')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
     list_filter = ('username', 'email', 'first_name', 'last_name', 'is_staff')
